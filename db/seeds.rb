0438a621216902ffb000a782fb31c53a7a792be5
yashktiwari18 <yashktiwari2003@gmail.com>
2026-07-04 11:59:33 +0530

    Add default admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Admin.find_or_create_by!(username: "admin") do |admin|
+  admin.name = "Administrator"
+  admin.password = "admin123"
+  admin.password_confirmation = "admin123"
+end
+
+puts "Admin created successfully!"
